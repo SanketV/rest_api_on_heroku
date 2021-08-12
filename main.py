619f7fe5242ca27b00afb2db51c8bf05e6ca7763
Sanket V <sanket.vaidya@jackson.com>
2021-08-12 17:16:27 -0500

    Postgres Detail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-books = [
+booksArr = [
     {
         "id": 1,
         "title": "Harry Potter and the Goblet of Fire",
@@ -17,21 +18,82 @@ books = [
 
 app = Flask(__name__)
 
+ENV = 'PROD'
+
+if ENV == 'dev':
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost/ApnaDB101'
+    app.debug = True
+else:
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://auoiprnvtnalbh:2baa9acd5b82e85631a8bbdeeb6f1829911a8029cd3ae0cbd6307138a62d0287@ec2-3-218-149-60.compute-1.amazonaws.com:5432/dfatnsjk7odsmv'
+    app.debug = False
+
+db = SQLAlchemy(app)
+
+class books(db.Model):
+    __tablename__ = 'books'
+    bookid = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(300), nullable = False)
+    authors = db.Column(db.String(200), nullable = False)
+    average_rating = db.Column(db.Integer())
+    isbn = db.Column(db.Integer())
+    language_code = db.Column(db.String(3))
+    num_pages = db.Column(db.Integer())
+    ratings_count = db.Column(db.Integer())
+    text_reviews_count = db.Column(db.Integer())
+    publication_date = db.Column(db.Date())
+    publisher = db.Column(db.String(100))
+
+    def __init__(self, bookid, title, authors, average_rating, isbn, isbn13, language_code, num_pages, ratings_count, text_reviews_count, publication_date, publisher):
+        self.bookid = bookid
+        self.title = title
+        self.authors = authors
+        self.average_rating = average_rating
+        self.isbn = isbn
+        self.isbn13 = isbn13
+        self.language_code = language_code
+        self.num_pages = num_pages
+        self.ratings_count = ratings_count
+        self.text_reviews_count = text_reviews_count
+        self.publication_date = publication_date
+        self.publisher = publisher
+
 @app.route("/")
 def index():
     return "Hola Mundo!"
 
 @app.route("/library/v1.0/books", methods=["GET"])
 def get_books():
-    return jsonify({"books": books})
+    return jsonify({"books": booksArr})
 
 @app.route("/library/v1.0/books/<int:book_id>", methods=["GET"])
 def get_book(book_id):
     result = {}
-    for book in books:
-        if book["id"] == book_id:
-            result = jsonify({"book": book})
+    #for book in books:
+    #    if book["id"] == book_id:
+    #        result = jsonify({"book": book})
     return result
+
+
+# ApnaDB01 - REST endpoints from postgres database
+@app.route("/library/v2.0/books", methods=["GET"])
+def get_books_db():
+    bookList = books.query.all()
+    result = []
+    for book in bookList:
+        currBook = {}
+        currBook['id'] = book.bookid
+        currBook['title'] = book.title
+        currBook['authors'] = book.authors
+        currBook['average_rating'] = book.average_rating
+        currBook['isbn'] = book.isbn
+        currBook['publication_date'] = book.publication_date
+        result.append(currBook)
+        print('• ' + str(currBook))
+
+    return jsonify({"books": result});
+
 
 if __name__ == "__main__":
     app.run()
